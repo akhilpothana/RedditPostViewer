a4ba773b54d50bd63c0fe0ce3d70581b52abc2d9
Akhil Pothana <apothana@datechnologies.co>
2019-09-12 00:53:40 -0400

    Created table view
    - Added table view to main view controller with stub data
    - Added text field and button for entering subreddit

---
+++ RedditPostViewer/RedditPostTableViewCell/RedditPostTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  RedditPostTableViewCell.swift
+//  RedditPostViewer
+//
+//  Created by Akhil Pothana on 9/12/19.
+//  Copyright © 2019 Akhil. All rights reserved.
+//
+
+import UIKit
+
+class RedditPostTableViewCell: UITableViewCell {
+    
+    @IBOutlet var postTitleLabel: UILabel!
+    @IBOutlet var subredditLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- RedditPostViewer/ViewController.swift
+++ RedditPostViewer/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var redditTableView: UITableView!
+    
 
+    var postDictionary = ["Title 1", "computers", "apple", "Title 4", "iPhone"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redditTableView.delegate = self
+        redditTableView.dataSource = self
+        registerNibs()
     }
-
-
+    
+    private func setup() {
+        
+    }
+    
+    // MARK: - Regiser Nibs
+    func registerNibs() {
+        let redditPostNib = UINib(nibName: String(describing: RedditPostTableViewCell.self), bundle: nil)
+        self.redditTableView.register(redditPostNib, forCellReuseIdentifier: String(describing: RedditPostTableViewCell.self))
+    }
+    
+    // MARK: - UITableViewDelegate and UITableViewDataSource methods
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let redditPostCell = tableView.dequeueReusableCell(withIdentifier: String(describing: RedditPostTableViewCell.self), for: indexPath) as? RedditPostTableViewCell else { return UITableViewCell() }
+        redditPostCell.postTitleLabel.text = postDictionary[indexPath.row]
+        
+        return redditPostCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
 }
 
